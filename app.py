6cd247ba9edd9aed92f1a8b8deab539d668e8611
Giada Unger <giada.unger02@gmail.com>
2024-10-02 10:51:59 +0200

    Added a function for fetching the reddit thread a user has inputed in the frontend

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
 import praw
 import tweepy
 import os
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app) 
 
@@ -12,6 +14,16 @@ reddit = praw.Reddit(
     client_secret=os.getenv("CLIENT_SECRET"),
     user_agent=os.getenv("USER_AGENT"),
 )
+
+def fetch_reddit_thread(url):
+    submission = reddit.submission(url=url)
+    submission.comments.replace_more(limit=0)
+    comments = [comment.body for comment in submission.comments.list()]
+    return {
+        "title": submission.title,
+        "author": submission.author.name,
+        "comments": comments
+    }
 
 client = tweepy.Client(bearer_token=os.getenv("TWITTER_TOKEN"))
 
@@ -23,13 +35,12 @@ def receive_input():
 
     if not data:
         return jsonify({'error': 'No data received'}), 400
-
-    response = {
-        'received_link': link,
-        'received_comment': comment
-    }
-
-    return jsonify(response), 200
+    
+    try:
+        reddit_data = fetch_reddit_thread(link)
+        return jsonify(reddit_data), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
