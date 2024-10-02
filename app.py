d580317b99a0dd77aa14570c2b28941242064c44
Giada Unger <giada.unger02@gmail.com>
2024-10-02 10:16:58 +0200

    Added env variables for the twitter and reddit api

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import praw
+import tweepy
+import os
 
 app = Flask(__name__)
 CORS(app) 
 
+reddit = praw.Reddit(
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    user_agent=os.getenv("USER_AGENT"),
+)
+
+client = tweepy.Client(bearer_token=os.getenv("TWITTER_TOKEN"))
+
 @app.route('/submit', methods=['POST'])
 def receive_input():
     data = request.json 
-    if not data:
-        return jsonify({'error': 'No data received'}), 400
-
     link = data.get('link')
     comment = data.get('comment')
+
+    if not data:
+        return jsonify({'error': 'No data received'}), 400
 
     response = {
         'received_link': link,
